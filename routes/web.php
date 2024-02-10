6f9e9c81df257bd755a804b778a566333d4aee54
Muhammad Nur Alfian Syarif <222112218@stis.ac.id>
2024-02-10 16:42:49 +0700

    refactor: web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PengirimanController;
+use App\Livewire\Beranda;
+use App\Livewire\CreatePengiriman;
+use App\Livewire\DetailPengiriman;
+use App\Livewire\EditPengiriman;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,30 +21,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-// Logout
-Route::post('logout', function () {
-    auth()
-        ->guard('web')
-        ->logout();
-
-    session()->invalidate();
-    session()->regenerateToken();
-
-    return redirect()->route('login');
-})->name('logout');
-
-Route::get('/pengiriman', App\Livewire\Beranda::class)->name('pengiriman')->middleware('permission:read-pengiriman');
-Route::get('/pengiriman/ajax', [App\Http\Controllers\PengirimanController::class, 'getPengiriman'])->name('pengiriman.ajax');
-Route::get('/pengiriman/create', App\Livewire\CreatePengiriman::class)->name('create.pengiriman')->middleware('role:admin|user');
-Route::get('/pengiriman/{pengirimanId}', App\Livewire\DetailPengiriman::class)->name('detail.pengiriman')->middleware('permission:read-pengiriman');
-Route::get('/pengiriman/{pengirimanId}/edit', App\Livewire\EditPengiriman::class)->name('edit.pengiriman')->middleware('role:admin');
+// Route::get('/pengiriman', Beranda::class)->name('pengiriman');
+// Route::get('/pengiriman/ajax', [PengirimanController::class, 'getPengiriman'])->name('pengiriman.ajax');
+// Route::get('/pengiriman/create', CreatePengiriman::class)->name('create.pengiriman')->middleware('role:admin|user');
+// Route::get('/pengiriman/{pengirimanId}', DetailPengiriman::class)->name('detail.pengiriman')->middleware('permission:read-pengiriman');
+// Route::get('/pengiriman/{pengirimanId}/edit', EditPengiriman::class)->name('edit.pengiriman')->middleware('role:admin');
 
 require __DIR__ . '/auth.php';
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('profile', 'profile')->name('profile');
+
+    Route::get('/pengiriman', Beranda::class)->name('pengiriman');
+    Route::get('/pengiriman/ajax', [PengirimanController::class, 'getPengiriman'])->name('pengiriman.ajax');
+
+    Route::middleware(['role:admin|user'])->group(function () {
+        Route::get('/pengiriman/create', CreatePengiriman::class)->name('create.pengiriman');
+    });
+
+    Route::middleware(['permission:read-pengiriman'])->group(function () {
+        Route::get('/pengiriman/{pengirimanId}', DetailPengiriman::class)->name('detail.pengiriman');
+    });
+
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/pengiriman/{pengirimanId}/edit', EditPengiriman::class)->name('edit.pengiriman');
+    });
+});
+
+// Logout route
+
+Route::post('logout', function () {
+    auth()->guard('web')->logout();
+    session()->invalidate();
+    session()->regenerateToken();
+    return redirect()->route('login');
+})->name('logout');
